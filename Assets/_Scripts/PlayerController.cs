2e051a841bb07cdfa2f3e2f43cdfa7c3d6aeb67d
Vovun <mrvovun@gmail.com>
2018-07-06 13:11:09 +0200

    Basic controller

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Movement();
+	}
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float veticalInput = Input.GetAxis("Vertical");
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+            transform.Translate(Vector3.up * Time.deltaTime * speed * veticalInput);
+        }
+    }
+
+    }

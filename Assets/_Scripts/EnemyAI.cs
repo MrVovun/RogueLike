eda9ade7b5d1211a5715547329dee78f4b082c32
Vovun <mrvovun@gmail.com>
2018-07-15 12:44:28 +0200

    Enemy controller using A* technology

--- Assets/_Scripts/EnemyAI.cs
+++ Assets/_Scripts/EnemyAI.cs
@@ -4,35 +4,14 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 
-    private GameObject player;
-
-    public float radius;
-    public int price;
-    public int speed;
-    private bool firstTime;
-
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        firstTime = true;
+
     }
 
     void Update()
     {
-        float distanceBetween = Vector2.Distance(this.transform.position, player.transform.position);
-        if (distanceBetween < radius && firstTime == true)
-        {
-            MoveTo();
-        }
-        else if (distanceBetween > radius && firstTime == false)
-        {
-            firstTime = true;
-        }
-    }
 
-    private void MoveTo()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, Time.deltaTime * speed);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -40,7 +19,6 @@ public class EnemyAI : MonoBehaviour {
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("We collided");
-            firstTime = false;
         }
     }
 }

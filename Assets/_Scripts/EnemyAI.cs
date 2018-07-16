27b37ed86a20487b1e14a78823147f3ce3d99fb6
Vovun <mrvovun@gmail.com>
2018-07-16 17:45:28 +0200

    Enemy radar for player, adapted enemy logic

--- Assets/_Scripts/EnemyAI.cs
+++ Assets/_Scripts/EnemyAI.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 
+    public float radius;    
+    public bool firstTime;
+
+    private int distanceBetween;
+    private PlayerController playerController;
+    private GameObject player;
+    private GameManager gameManager;
+    [SerializeField]
+    private AILerp lerp;
+
     void Start()
     {
-
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        firstTime = true;
+        lerp.canMove = false;
     }
 
     void Update()
     {
+        float distanceBetween = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distanceBetween < radius && firstTime == true)
+        {
+            lerp.canMove = true;
+        }
+        else if (distanceBetween > radius && firstTime == false)
+        {
+            firstTime = true;
+        }
 
     }
 
@@ -19,6 +44,8 @@ public class EnemyAI : MonoBehaviour {
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("We collided");
+            firstTime = false;
+            lerp.canMove = false;
         }
     }
 }
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,7 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public bool pause;
+
+    private void Start()
+    {
+        pause = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && pause == false)
+        {
+            PauseGame();
+            pause = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && pause == true)
+        {
+            UnpauseGame();
+            pause = false;
+        }
+    }
 
 
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void UnpauseGame()
+    {
+        Time.timeScale = 1;
+    }
 }
